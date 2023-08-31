17feb24f7846e9f1f774a4d4f7baa45fc4c48369
Mutaganda Elysee <mutagandaelysee2@gmail.com>
2023-08-31 11:33:05 +0000

    0x1D. C - Binary trees | part 3

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,9 +6,15 @@
 */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left = 0, right = 0;
+
 	if (tree == NULL)
 		return (0);
-	return (binary_tree_height2(tree->left) - binary_tree_height2(tree->right));
+	if (tree->left)
+		left = binary_tree_height2(tree->left) + 1;
+	if (tree->right)
+		right = binary_tree_height2(tree->right) + 1;
+	return (left - right);
 }
 /**
 * binary_tree_height2 - a function that mesures the hight of a binary tree
@@ -18,7 +24,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 size_t binary_tree_height2(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 	return (big(binary_tree_height2(tree->left),
@@ -32,7 +38,7 @@ size_t binary_tree_height2(const binary_tree_t *tree)
 */
 size_t big(size_t value1, size_t value2)
 {
-	if (value1 > value2)
+	if (value1 >= value2)
 		return (value1);
 	else
 		return (value2);
