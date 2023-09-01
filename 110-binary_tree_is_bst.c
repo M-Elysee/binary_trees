4c7b7d46bfb232a0dec772901b082c60358a75fb
WillingtonM <willington.mhlanga@gmail.com>
2023-09-01 15:33:02 +0200

    0x1D. C - Binary trees | debug 4

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,36 +1,69 @@
 #include "binary_trees.h"
+#include <stdio.h>
 
 /**
- * is_bst - Compare node in order to check if given
+ * greater_thn - check if all values in tree are greater than value
+ * @tree: Pointer to tree to check
+ * @val: Value to check against
  *
- * @tree: Pointer to root node of tree to check
- * @min: Minimum value
- * @max: Maximum value
- *
- * Return: Returns 1 if success, otherwise returns 0
+ * Return: Returns 1 if true otherwise 0 if false
  */
-int is_bst(const binary_tree_t *tree, int min, int max)
+int greater_thn(const binary_tree_t *tree, int val)
 {
+	int lft, rght;
+
 	if (tree == NULL)
 		return (1);
-
-	if (tree->n < min || tree->n > max)
-		return (0);
-
-	return (
-		is_bst(tree->left, min, tree->n - 1) &&
-		is_bst(tree->right, tree->n + 1, max));
+	if (tree->n > val)
+	{
+		lft = greater_thn(tree->left, val);
+		rght = greater_thn(tree->right, val);
+		if (lft && rght)
+			return (1);
+	}
+	return (0);
 }
 
 /**
- * binary_tree_is_bst - Checks if a binary tree is a valid Binary Search tree
+ * less_thn - check if all values in tree are less than a specific value
+ * @tree: Pointer to tree to check
+ * @val: Value to check against
  *
- * @tree: Pointer to root node of tree to check
- * Return: Returns 1 if success, otherwise returns 0
+ * Return: Returns 1 if true otherwise returns 0 if false
+ */
+int less_thn(const binary_tree_t *tree, int val)
+{
+	int lft, rght;
+
+	if (tree == NULL)
+		return (1);
+	if (tree->n < val)
+	{
+		lft = less_thn(tree->left, val);
+		rght = less_thn(tree->right, val);
+		if (lft && rght)
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid binary search tree
+ * @tree: Pointer to root node of the tree to check
+ *
+ * Return: Returns 1 if tree is a valid BST, Othewise returns  0.If tree is NULL, return 0
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (is_bst(tree, INT_MIN, INT_MAX));
+	if (less_thn(tree->left, tree->n) && greater_thn(tree->right, tree->n))
+	{
+		if (!tree->left || binary_tree_is_bst(tree->left))
+		{
+			if (!tree->right || binary_tree_is_bst(tree->right))
+				return (1);
+		}
+	}
+	return (0);
 }
--- 130-binary_tree_is_heap.c
+++ 130-binary_tree_is_heap.c
@@ -1,114 +1,85 @@
 #include "binary_trees.h"
+#include <stdlib.h>
 
 /**
- * binary_tree_is_heap - checks if binary tree is valid Max Binary Heap
- * @tree: Tree to check
- * Return: Returns 1 is it is orotherwise
+ * bt_size - measures size of a binary tree
+ *
+ * @tree: tree root
+ * Return: Returns size of tree or 0 if tree is NULL;
+ */
+size_t bt_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (bt_size(tree->left) + bt_size(tree->right) + 1);
+}
+
+/**
+ * tree_is_complete - checks if tree is complete
+ *
+ * @tree: Pointer to tree root
+ * @i: Node index
+ * @cnodes: Number of nodes
+ * Return: Returns 1 if tree is complete otherwise returns 0
+ */
+int tree_is_complete(const binary_tree_t *tree, int i, int cnodes)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (i >= cnodes)
+		return (0);
+
+	return (tree_is_complete(tree->left, (2 * i) + 1, cnodes) &&
+		tree_is_complete(tree->right, (2 * i) + 2, cnodes));
+}
+
+/**
+ * bt_is_complete - calls to tree_is_complete function
+ *
+ * @tree: tree root
+ * Return: Returns 1 if tree is complete otherwise returns 0
+ */
+int bt_is_complete(const binary_tree_t *tree)
+{
+	size_t c_nodes;
+
+	if (tree == NULL)
+		return (0);
+
+	c_nodes = bt_size(tree);
+
+	return (tree_is_complete(tree, 0, c_nodes));
+}
+
+/**
+ * parent_check - checks if parent has a greater value than its childs
+ *
+ * @tree: Pointer to node
+ * Return: Returns 1 if parent has a greater value otherwise 0
+ */
+int parent_check(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (tree->n > tree->parent->n)
+		return (0);
+
+	return (parent_check(tree->left) && parent_check(tree->right));
+}
+
+/**
+ * binary_tree_is_heap - checks if input tree is Max Binary Heap
+ *
+ * @tree: Pointer to root of the tree
+ * Return: Returns 1 if tree is Max Binary Heap otherwise returns 0
  */
 int binary_tree_is_heap(const binary_tree_t *tree)
 {
-	int bal_val;
-
-	if (tree == NULL)
+	if (!bt_is_complete(tree))
 		return (0);
-	if (tree->left && tree->left->n > tree->n)
-		return (0);
-	if (tree->right && tree->right->n > tree->n)
-		return (0);
-	if (bt_is_perfect(tree))
-		return (1);
-	bal_val = bt_balance(tree);
-	if (bal_val == 0)
-		return (bt_is_perfect(tree->left)
-			&& binary_tree_is_heap(tree->right));
-	if (bal_val == 1)
-		return (binary_tree_is_heap(tree->left)
-			&& bt_is_perfect(tree->right));
-	else
-		return (0);
-}
 
-/**
- * tree_perfect - if a tree is perfect or not
- * @tree: Tree to check
- * Return: Returns 0 if not perfect or other number that is level of height
- */
-int tree_perfect(const binary_tree_t *tree)
-{
-	int lft = 0, rgt = 0;
-
-	if (tree->left && tree->right)
-	{
-		lft = 1 + tree_perfect(tree->left);
-		rgt = 1 + tree_perfect(tree->right);
-		if (rgt == lft && rgt != 0 && lft != 0)
-			return (rgt);
-		return (0);
-	}
-	else if (!tree->left && !tree->right)
-		return (1);
-	else
-		return (0);
-}
-
-/**
- * bt_balance - Measures balance factor of binary tree
- * @tree: Tree to go through
- * Return: returns balanced factor
- */
-int bt_balance(const binary_tree_t *tree)
-{
-	int rgt = 0, lft = 0, total = 0;
-
-	if (tree)
-	{
-		lft = ((int)bt_height(tree->left));
-		rgt = ((int)bt_height(tree->right));
-		total = lft - rgt;
-	}
-	return (total);
-}
-
-/**
- * bt_is_perfect - perfect or not a tree
- * @tree: Tree to check
- * Return: returns 1 if it is or 0 otherwise
- */
-int bt_is_perfect(const binary_tree_t *tree)
-{
-	int res = 0;
-
-	if (tree == NULL)
-		return (0);
-	res = tree_perfect(tree);
-	if (res != 0)
-		return (1);
-	return (0);
-}
-
-/**
- * bt_height - Measures height of binary tree
- * @tree: Tree to go through
- * Return: Returns height
- */
-size_t bt_height(const binary_tree_t *tree)
-{
-	size_t lft = 0;
-	size_t rgt = 0;
-
-	if (tree == NULL)
-	{
-		return (0);
-	}
-	else
-	{
-		if (tree->left == NULL && tree->right == NULL)
-			return (tree->parent != NULL);
-		if (tree)
-		{
-			lft = tree->left ? 1 + bt_height(tree->left) : 0;
-			rgt = tree->right ? 1 + bt_height(tree->right) : 0;
-		}
-		return ((lft > rgt) ? lft : rgt);
-	}
+	return (parent_check(tree->left) && parent_check(tree->right));
 }
